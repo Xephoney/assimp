5935c19b46698d9776527f757e593547e6b6fcaf
Hans Ola Hoftun <hans_olahoftun@hotmail.com>
2022-11-07 00:20:44 +0100

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,34 @@
+project "assimp"
+	kind "SharedLib"
+	language "C++"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/**.h",
+		"include/**.c",
+		"include/**.inl",
+		"include/**.hpp",
+		"include/**.cpp"
+	}
+
+
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
